58c04debe00a3bbe7c12d743003c4d636181238d
Dlyf <domilung@gmail.com>
2022-05-17 11:09:08 +0200

    js

--- programme.js
+++ programme.js
@@ -1,5 +1,34 @@
 // Phase test
+// function fibonacciGenerator(n) {
+//     let Output = [];
+//     if(n === 1) {
+//         Output = [0];
+//         // return Output;
+//     } else if(n === 2) {
+//         Output = [0, 1];
+//     } else {
+//         Output = [0, 1];
+//         for (let i = 2; i<n; i++) {
+//             Output.push(Output[Output.length - 2] + Output[Output.length - 1]);
+//         }
+//     }
+//     return Output;
 
-let phrase = "Le débogueur c'est ";
+// }
+
+// Output = fibonacciGenerator(15)
+// console.log(Output);
+
+let phrase = "hello";
 let adjectif = "super";
 console.log(phrase + adjectif);
+
+// utiliser un point d'arrêt
+// . réexécuter avec F5 le débogueur
+// la flèche du bas c'est pour rentrer dans les fonctons
+// Exécuter le programme jusqu'au prochain point d'arrêt
+// Isoler les variables qui nous intéressent et les mettres dans la section espion
+// On peut survoler directement dans le code source
+// Modifier la variable à la volée pendant que le programme s'exécute : 
+// aller dans variable et modifier la valeur du variable
+// Launch programme : ${file}
